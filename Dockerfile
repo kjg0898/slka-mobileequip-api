2417a235e4380dea0bfe57136de1e70c6ae0af7c
kjg0898 <kjg0898@gmail.com>
2024-06-19 15:21:09 +0900

    배포 작업 완료

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+# Use an official OpenJDK runtime as a parent image
+FROM openjdk:17-jdk
+
+# Set environment variables
+ENV TZ=Asia/Seoul
+ENV APP_HOME=/usr/app/
+
+# Create application directory
+WORKDIR $APP_HOME
+
+# Copy the jar file into the container
+COPY build/libs/srlk-mobileequip-api-1.0-jar-with-dependencies.jar app.jar
+
+# Copy the configuration files into the container
+COPY src/main/resources/application.properties .
+COPY src/main/resources/application-test.properties .
+COPY src/main/resources/application-prod.properties .
+COPY src/main/resources/logback.xml .
+
+
+# Run the application when the container launches
+ENTRYPOINT ["sh", "-c", "java -jar app.jar"]
---
+++ build_and_push.bat
@@ -0,0 +1,54 @@
+@echo off
+SETLOCAL
+
+REM 환경 변수 설정
+SET DOCKER_USERNAME=admin
+SET DOCKER_PASSWORD=nbr_gg123!@#
+SET DOCKER_REGISTRY=211.189.132.16:5000
+
+REM 경로 설정
+SET PROJECT_PATH=%~dp0
+SET DOCKER_IMAGE=%DOCKER_REGISTRY%/srlk/srlk-mobileequip-api-test:0.1
+
+REM Docker 데몬 실행 확인
+echo Checking Docker daemon...
+docker info
+IF %ERRORLEVEL% NEQ 0 (
+    echo Docker daemon is not running. Please start Docker Desktop.
+    PAUSE
+    EXIT /B 1
+)
+
+REM Docker 이미지 생성
+echo Building Docker image...
+docker build -t %DOCKER_IMAGE% %PROJECT_PATH%
+IF %ERRORLEVEL% NEQ 0 (
+    echo Docker image build failed.
+    PAUSE
+    EXIT /B 1
+)
+
+REM Docker 로그인
+echo Logging in to Docker...
+docker login %DOCKER_REGISTRY% -u %DOCKER_USERNAME% -p %DOCKER_PASSWORD%
+IF %ERRORLEVEL% NEQ 0 (
+    echo Docker login failed.
+    PAUSE
+    EXIT /B 1
+)
+
+REM Docker 이미지 푸시
+echo Pushing Docker image...
+docker push %DOCKER_IMAGE%
+IF %ERRORLEVEL% NEQ 0 (
+    echo Docker image push failed.
+    PAUSE
+    EXIT /B 1
+)
+
+REM 스크립트 완료 메시지
+echo Docker image build and push completed successfully.
+
+ENDLOCAL
+@echo on
+PAUSE
